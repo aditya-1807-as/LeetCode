a296d716dc502a19a81aeab7fbb85d5d9152f6bd
Aditya Asthana <adityaasthana1807@gmail.com>
2026-07-12 17:29:14 +0530

    Time: 0 ms (100%), Space: 119.1 MB (52.25%) - LeetHub

--- 1901-find-a-peak-element-ii/1901-find-a-peak-element-ii.java
+++ 1901-find-a-peak-element-ii/1901-find-a-peak-element-ii.java
@@ -1,20 +1,64 @@
+// class Solution {
+//     public int[] findPeakGrid(int[][] mat) {
+
+//         int m = mat.length;
+//         int n = mat[0].length;
+
+//         for (int i = 0; i < m; i++) {
+//             for (int j = 0; j < n; j++) {
+
+//                 boolean up = (i == 0) || mat[i][j] > mat[i - 1][j];
+//                 boolean down = (i == m - 1) || mat[i][j] > mat[i + 1][j];
+//                 boolean left = (j == 0) || mat[i][j] > mat[i][j - 1];
+//                 boolean right = (j == n - 1) || mat[i][j] > mat[i][j + 1];
+
+//                 if (up && down && left && right) {
+//                     return new int[]{i, j};
+//                 }
+//             }
+//         }
+
+//         return new int[]{-1, -1};
+//     }
+// }
+
+
 class Solution {
+
     public int[] findPeakGrid(int[][] mat) {
 
-        int m = mat.length;
-        int n = mat[0].length;
+        int rows = mat.length;
+        int cols = mat[0].length;
 
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
+        int left = 0;
+        int right = cols - 1;
 
-                boolean up = (i == 0) || mat[i][j] > mat[i - 1][j];
-                boolean down = (i == m - 1) || mat[i][j] > mat[i + 1][j];
-                boolean left = (j == 0) || mat[i][j] > mat[i][j - 1];
-                boolean right = (j == n - 1) || mat[i][j] > mat[i][j + 1];
+        while (left <= right) {
 
-                if (up && down && left && right) {
-                    return new int[]{i, j};
+            int mid = left + (right - left) / 2;
+
+            int maxRow = 0;
+
+            // Find maximum element in mid column
+            for (int i = 1; i < rows; i++) {
+                if (mat[i][mid] > mat[maxRow][mid]) {
+                    maxRow = i;
                 }
+            }
+
+            int leftValue = (mid > 0) ? mat[maxRow][mid - 1] : -1;
+            int rightValue = (mid < cols - 1) ? mat[maxRow][mid + 1] : -1;
+
+            if (mat[maxRow][mid] > leftValue &&
+                mat[maxRow][mid] > rightValue) {
+
+                return new int[]{maxRow, mid};
+            }
+
+            if (leftValue > mat[maxRow][mid]) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
             }
         }
 
