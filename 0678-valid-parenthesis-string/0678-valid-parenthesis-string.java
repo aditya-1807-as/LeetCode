05f43d4a1dc02e9784ddae716a43211199fdffed
Aditya Asthana <adityaasthana1807@gmail.com>
2026-08-10 21:32:55 +0530

    Time: 0 ms (100%), Space: 42.9 MB (35.06%) - LeetHub

---
+++ 0678-valid-parenthesis-string/0678-valid-parenthesis-string.java
@@ -0,0 +1,30 @@
+class Solution {
+    public boolean checkValidString(String s) {
+        int min = 0;
+        int max = 0;
+
+        for (char c : s.toCharArray()) {
+
+            if (c == '(') {
+                min++;
+                max++;
+            } 
+            else if (c == ')') {
+                min--;
+                max--;
+            } 
+            else { // '*'
+                min--; // '*' as ')'
+                max++; // '*' as '('
+            }
+
+            if (max < 0)
+                return false;
+
+            if (min < 0)
+                min = 0;
+        }
+
+        return min == 0;
+    }
+}
