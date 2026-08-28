0697b2d2ff73acb0b4dfc20cf502c230a047c690
Aditya Asthana <adityaasthana1807@gmail.com>
2026-08-28 22:34:56 +0530

    Time: 2 ms (31.7%), Space: 42.8 MB (69.67%) - LeetHub

--- 1046-last-stone-weight/1046-last-stone-weight.java
+++ 1046-last-stone-weight/1046-last-stone-weight.java
@@ -1,25 +1,48 @@
-import java.util.Collections;
-import java.util.PriorityQueue;
+// import java.util.Collections;
+// import java.util.PriorityQueue;
 
+// class Solution {
+//     public int lastStoneWeight(int[] stones) {
+//         PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder());
+
+//         // Add all stones to the max heap
+//         for (int stone : stones) {
+//             maxHeap.offer(stone);
+//         }
+
+//         // Smash the two largest stones
+//         while (maxHeap.size() > 1) {
+//             int first = maxHeap.poll();
+//             int second = maxHeap.poll();
+
+//             if (first != second) {
+//                 maxHeap.offer(first - second);
+//             }
+//         }
+
+//         return maxHeap.isEmpty() ? 0 : maxHeap.poll();
+//     }
+// }
 class Solution {
     public int lastStoneWeight(int[] stones) {
-        PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder());
 
-        // Add all stones to the max heap
+        PriorityQueue<Integer> pq =
+            new PriorityQueue<>(Collections.reverseOrder());
+
         for (int stone : stones) {
-            maxHeap.offer(stone);
+            pq.add(stone);
         }
 
-        // Smash the two largest stones
-        while (maxHeap.size() > 1) {
-            int first = maxHeap.poll();
-            int second = maxHeap.poll();
+        while (pq.size() > 1) {
 
-            if (first != second) {
-                maxHeap.offer(first - second);
+            int a = pq.poll();  // heaviest
+            int b = pq.poll();  // second heaviest
+
+            if (a != b) {
+                pq.add(a - b);
             }
         }
 
-        return maxHeap.isEmpty() ? 0 : maxHeap.poll();
+        return pq.isEmpty() ? 0 : pq.peek();
     }
 }
