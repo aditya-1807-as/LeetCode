b9e3216bfaab6265798a90820e263e0467db6577
Aditya Asthana <adityaasthana1807@gmail.com>
2026-09-17 20:36:41 +0530

    Time: 7 ms (56.82%) | Memory: 47.2 MB (48.70%) - LeetSync

---
+++ 207-course-schedule/course-schedule.java
@@ -0,0 +1,52 @@
+import java.util.*;
+
+class Solution {
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+
+        List<List<Integer>> graph = new ArrayList<>();
+
+        for (int i = 0; i < numCourses; i++) {
+            graph.add(new ArrayList<>());
+        }
+
+        int[] indegree = new int[numCourses];
+
+        // Build graph
+        for (int[] prerequisite : prerequisites) {
+            int course = prerequisite[0];
+            int pre = prerequisite[1];
+
+            graph.get(pre).add(course);
+            indegree[course]++;
+        }
+
+        // Courses with no prerequisites
+        Queue<Integer> queue = new LinkedList<>();
+
+        for (int i = 0; i < numCourses; i++) {
+            if (indegree[i] == 0) {
+                queue.offer(i);
+            }
+        }
+
+        int count = 0;
+
+        // Topological Sort
+        while (!queue.isEmpty()) {
+
+            int current = queue.poll();
+            count++;
+
+            for (int next : graph.get(current)) {
+
+                indegree[next]--;
+
+                if (indegree[next] == 0) {
+                    queue.offer(next);
+                }
+            }
+        }
+
+        return count == numCourses;
+    }
+}
